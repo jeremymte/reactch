9156d2fd6f98c1827c4d3270a4b7e595b37da802
Jeremy Te <jayte718@gmail.com>
2019-09-03 19:52:57 -0400

    Import React

--- app.js
+++ app.js
@@ -1,22 +1,12 @@
-// lesson 8
+// lesson 9
 
 import React from 'react';
-import BookList from './components/BookList';
-import Navbar from './components/Navbar';
-import ThemeContextProvider from './contexts/ThemeContext';
-import ThemeToggle from './components/ThemeToggle';
-import AuthContextProvider from './contexts/AuthContext';
+import SongList from './components/SongList';
 
 function App() {
   return (
     <div className="App">
-      <ThemeContextProvider>
-        <AuthContextProvider>
-          <Navbar />
-          <BookList />
-          <ThemeToggle />
-        </AuthContextProvider>
-      </ThemeContextProvider>
+      <SongList />
     </div>
   );
 }
